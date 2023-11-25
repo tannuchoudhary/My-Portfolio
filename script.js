0dd17fce1a002f5f4d1da2fbd62d7169b07ff3d5
TANNU KUMARI <tannuchoudhary10@gmail.com>
2023-11-25 21:14:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Full-Stack-Developer",  "Competitive-Programmer", "Problem-Solver", "Chess-Player", "Freelancer"],
+        strings: ["Software Engineer",  "Competitive-Programmer", "Problem-Solver", "Chess-Player"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student","Full-Stack-Developer", "Competitive-Programmer", "Problem-Solver", "Chess-Player", "Freelancer"],
+        strings: ["Software Engineer", "Competitive-Programmer", "Problem-Solver", "Chess-Player"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
